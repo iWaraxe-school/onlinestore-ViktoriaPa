0ed66e77781181db1b9ae933685b13707d72d274
viktoriapashkovskaya <vika.vabishchevich.96@mail.ru>
2022-10-09 18:19:08 +0200

    The XML document is parsed

--- consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
+++ consoleApp/src/main/java/by/issoft/consoleApp/StoreApp.java
@@ -1,9 +1,16 @@
 package by.issoft.consoleApp;
+import by.issoft.domain.XMLparser.XMLParser;
 import by.issoft.store.Store;
 import by.issoft.store.storeHelper.RandomStorePopulator;
 
+import java.util.Map;
+
 public class StoreApp {
     public static void main(String[] args) {
+        XMLParser parser = new XMLParser();
+        Map<String, String> typesOfSorting = parser.parse();
+        System.out.println(typesOfSorting);
+
         Store storeWithCategoriesAndProducts = new Store();
         RandomStorePopulator populator = new RandomStorePopulator(storeWithCategoriesAndProducts);
         populator.generateRandomStore();
